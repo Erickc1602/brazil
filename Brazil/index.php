69606d6816d9cf72658be10ec21b0135008d1286
Erickc1602 <ecarv97@gmail.com>
2023-02-05 02:44:45 -0500

    final

--- Brazil/index.php
+++ Brazil/index.php
@@ -1,6 +1,1 @@
 <?php include_once("index.html"); ?>
-<?php include_once("1958.html"); ?>
-<?php include_once("1962.html"); ?>
-<?php include_once("1970.html"); ?>
-<?php include_once("1994.html"); ?>
-<?php include_once("2002.html"); ?>
